4ea822e111f221bab1711cbbfc6391e35f90aea3
boxhezi <547181293@qq.com>
2024-11-09 19:21:36 +0800

    support password hash file with multiple password hash, line by line

--- werkzeug_cracker.py
+++ werkzeug_cracker.py
@@ -12,26 +12,25 @@ __version__ = "1.5"
 __author__ = "Anataar"
 
 class Werkzeug_Cracker(threading.Thread):
-
     def __init__(self, hash, password_lst, workers):
         super().__init__()
-        self.hash = hash[0]
+        self.hash = hash
         self.password_lst = password_lst
         self.workers = workers
         self._stop_event = threading.Event()
-        self.bar = IncrementalBar('Countdown', max = password_lst.qsize())
-        self.start()
+        self.bar = IncrementalBar(f"Cracking {self.hash}", max = password_lst.qsize())
 
     def init_workers(self):
         events = []
 
         ## Start Workers
-        for i in range(self.workers):
+        for _ in range(self.workers):
             event = threading.Thread(target=self.brute)
             events.append(event)
-            events[-1].start()
+            # events[-1].start()
 
-        return True
+        return events
+        # return True
 
     ## Stop workers
     def stop(self):
@@ -44,21 +43,24 @@ class Werkzeug_Cracker(threading.Thread):
         return self._stop_event.is_set()
 
     def brute(self):
-
         ## While the Queue isn't empty
         while not self.password_lst.empty() and self.stopped() != True:
             password = self.password_lst.get()
             self.bar.next()
-            
+
             ## Will check if the password match
-            if check_password_hash(self.hash, password) == True:
+            if check_password_hash(self.hash, password):
                 self.stop()
                 sys.stdout.write(f"\nPassword found: {password}")
         sys.exit(0)
 
-
     def run(self):
-        self.init_workers()
+        events = self.init_workers()
+        for e in events:
+            e.start()
+
+        for e in events:
+            e.join()
         return True
 
 
@@ -79,15 +81,10 @@ if __name__ == "__main__":
         sys.stdout.write(parser.format_help())
 
     else:
-        ## Open the hash file
-        with open(args.password,"r") as hash:
-            hash =  hash.read().split()
-        
         ## Open the wordlist
-        with open(args.wordlist,"r", encoding="latin-1") as wordlist:
-
+        with open(args.wordlist, "r", encoding="latin-1") as wordlist:
             raw_words = wordlist.read().split()
-            
+
             ## Initialize the Queue
             words = Queue()
 
@@ -95,6 +92,15 @@ if __name__ == "__main__":
             for word in raw_words:
                 words.put(word)
 
-            
+        ## Open the hash file
+        with open(args.password,"r") as phash:
+            hashes = phash.read().split()
+
         ## Initialize
-        Werkzeug_Cracker(hash, words, args.threads)
+        for h in hashes:
+            cracker = Werkzeug_Cracker(h, words, args.threads)
+            cracker.start()
+            cracker.join()
+        # Werkzeug_Cracker(hash, words, args.threads)
+        # for i in range(len(hashes)):
+        #     Werkzeug_Cracker(hashes[i], words, args.threads)
